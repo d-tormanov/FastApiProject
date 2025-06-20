f477ddf95108089acc0a64f5b1f8af8365b8cb6f
d-tormanov <dtormanov@gmail.com>
2025-06-20 11:13:32 +0300

    Create migrate.py

---
+++ migrate.py
@@ -0,0 +1,11 @@
+import asyncio
+from alembic.config import Config
+from alembic import command
+
+alembic_cfg = Config("alembic.ini")
+
+async def run():
+    command.upgrade(alembic_cfg, "head")
+
+if __name__ == "__main__":
+    asyncio.run(run())

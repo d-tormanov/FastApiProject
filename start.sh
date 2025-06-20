8662e43363c0111b3efcaf3856540663e05fd511
d-tormanov <dtormanov@gmail.com>
2025-06-20 13:51:40 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p alembic/versions
+
+echo "Генерация миграций Alembiс..."
+alembic revision --autogenerate -m "autogen" || echo "📭 Нет изменений для миграции"
+
+echo "Применение миграций Alembic..."
+alembic upgrade head
+
+echo "Запуск FastAPI через Uvicorn..."
+exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}

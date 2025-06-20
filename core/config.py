0ccd6565bad3730338e406b29f24b78573fb5ada
d-tormanov <dtormanov@gmail.com>
2025-06-20 11:50:48 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -2,19 +2,19 @@ from pydantic_settings import BaseSettings
 
 
 class AppSettings(BaseSettings):
-    postgres_user: str
-    postgres_password: str
-    postgres_host: str
-    postgres_port: int
-    postgres_db: str
+    # postgres_user: str
+    # postgres_password: str
+    # postgres_host: str
+    # postgres_port: int
+    # postgres_db: str
     postgres_url: str
 
-    @property
-    def postgres_dsn(self) -> str:
-        return (
-            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
-            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
-        )
+    # @property
+    # def postgres_dsn(self) -> str:
+    #     return (
+    #         f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
+    #         f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+    #     )
 
     class Config:
         env_file = ".env"

63541151ac845362655e3274cc717574d051dd7c
d-tormanov <dtormanov@gmail.com>
2025-06-20 12:01:50 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -9,12 +9,13 @@ class AppSettings(BaseSettings):
     # postgres_db: str
     postgres_url: str
 
-    # @property
-    # def postgres_dsn(self) -> str:
-    #     return (
-    #         f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
-    #         f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
-    #     )
+    @property
+    def postgres_dsn(self) -> str:
+        return self.postgres_url
+        # return (
+        #     f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
+        #     f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        # )
 
     class Config:
         env_file = ".env"

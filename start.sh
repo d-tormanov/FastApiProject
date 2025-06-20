d7ebab2110e198061277f195bbf9f0af120189ac
d-tormanov <dtormanov@gmail.com>
2025-06-20 14:02:23 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ echo "Применение миграций Alembic..."
 alembic upgrade head
 
 echo "Запуск FastAPI через Uvicorn..."
-exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
+exec uvicorn main:app --host 0.0.0.0 --port 8000

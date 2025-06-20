25b743fc348420ed8fea1c5f0561b2e923775a38
d-tormanov <dtormanov@gmail.com>
2025-06-20 12:18:30 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -7,11 +7,11 @@ class AppSettings(BaseSettings):
     # postgres_host: str
     # postgres_port: int
     # postgres_db: str
-    postgres_url: str
+    database_url: str
 
     @property
     def postgres_dsn(self) -> str:
-        return self.postgres_url
+        return self.database_url
         # return (
         #     f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
         #     f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
